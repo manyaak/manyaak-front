da8016bf00885087ea20799900f222c5bc14d1a2
kanghyun98 <lkh981107@gmail.com>
2023-07-03 01:59:21 +0900

    feat: add constants

---
+++ constants/styles/color.ts
@@ -0,0 +1,19 @@
+export const COLORS = {
+  primary: {
+    yellow: '#FFFB7D',
+  },
+  grayscale: {
+    white: '#FFFFFF',
+    gray50: '#F8F9FA',
+    gray100: '#F3F5F7',
+    gray200: '#ECEEF0',
+    gray300: '#E2E5E8',
+    gray400: '#D4D8DC',
+    gray500: '#B5B9BD',
+    gray600: '#8E9398',
+    gray700: '#505458',
+    gray800: '#3A3D40',
+    gray900: '#252729',
+    black: '#000000',
+  },
+};
