1d4ca2ed9336a4679fe1afd6d114e3155d4cffa8
ballsona <ballsona0925@naver.com>
2023-07-06 00:45:10 +0900

    style: modify type of textStyles

--- src/constants/styles/textStyles.ts
+++ src/constants/styles/textStyles.ts
@@ -26,7 +26,7 @@ export type TextStyleNameType =
 interface TextStyleType {
   fontSize: number;
   fontWeight: number;
-  lineHeight: number;
+  lineHeight: string;
 }
 
 const LINE_HEIGHT_RATIO = 1.5;
@@ -35,96 +35,96 @@ export const TEXT_STYLES: Record<TextStyleNameType, TextStyleType> = {
   [TEXT_STYLE_NAME.title]: {
     fontSize: 30,
     fontWeight: 700,
-    lineHeight: 30 * LINE_HEIGHT_RATIO,
+    lineHeight: `${30 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.title2]: {
     fontSize: 25,
     fontWeight: 700,
-    lineHeight: 25 * LINE_HEIGHT_RATIO,
+    lineHeight: `${25 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.title3]: {
     fontSize: 22,
     fontWeight: 700,
-    lineHeight: 22 * LINE_HEIGHT_RATIO,
+    lineHeight: `${22 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.subtitle1]: {
     fontSize: 22,
     fontWeight: 600,
-    lineHeight: 22 * LINE_HEIGHT_RATIO,
+    lineHeight: `${22 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.subtitle2B]: {
     fontSize: 20,
     fontWeight: 600,
-    lineHeight: 20 * LINE_HEIGHT_RATIO,
+    lineHeight: `${20 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.subtitle2R]: {
     fontSize: 20,
     fontWeight: 500,
-    lineHeight: 20 * LINE_HEIGHT_RATIO,
+    lineHeight: `${20 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.subtitle3]: {
     fontSize: 18,
     fontWeight: 600,
-    lineHeight: 18 * LINE_HEIGHT_RATIO,
+    lineHeight: `${18 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.body1B]: {
     fontSize: 16,
     fontWeight: 600,
-    lineHeight: 16 * LINE_HEIGHT_RATIO,
+    lineHeight: `${16 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.body1R]: {
     fontSize: 16,
     fontWeight: 400,
-    lineHeight: 16 * LINE_HEIGHT_RATIO,
+    lineHeight: `${16 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.body2B]: {
     fontSize: 14,
     fontWeight: 600,
-    lineHeight: 14 * LINE_HEIGHT_RATIO,
+    lineHeight: `${14 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.body2R]: {
     fontSize: 14,
     fontWeight: 400,
-    lineHeight: 14 * LINE_HEIGHT_RATIO,
+    lineHeight: `${14 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.body3]: {
     fontSize: 13,
     fontWeight: 400,
-    lineHeight: 13 * LINE_HEIGHT_RATIO,
+    lineHeight: `${13 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.button1B]: {
     fontSize: 16,
     fontWeight: 500,
-    lineHeight: 16 * LINE_HEIGHT_RATIO,
+    lineHeight: `${16 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.button1R]: {
     fontSize: 16,
     fontWeight: 400,
-    lineHeight: 16 * LINE_HEIGHT_RATIO,
+    lineHeight: `${16 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.button2B]: {
     fontSize: 14,
     fontWeight: 500,
-    lineHeight: 14 * LINE_HEIGHT_RATIO,
+    lineHeight: `${14 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.button2R]: {
     fontSize: 14,
     fontWeight: 400,
-    lineHeight: 14 * LINE_HEIGHT_RATIO,
+    lineHeight: `${14 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.button3]: {
     fontSize: 13,
     fontWeight: 400,
-    lineHeight: 13 * LINE_HEIGHT_RATIO,
+    lineHeight: `${13 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.caption1]: {
     fontSize: 12,
     fontWeight: 400,
-    lineHeight: 12 * LINE_HEIGHT_RATIO,
+    lineHeight: `${12 * LINE_HEIGHT_RATIO}px`,
   },
   [TEXT_STYLE_NAME.caption2]: {
     fontSize: 12,
     fontWeight: 300,
-    lineHeight: 12 * LINE_HEIGHT_RATIO,
+    lineHeight: `${12 * LINE_HEIGHT_RATIO}px`,
   },
 };
