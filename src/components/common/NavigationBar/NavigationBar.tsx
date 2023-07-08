9bf4da3d44418a4673007c6bba70de83e34b4bc9
kanghyun98 <lkh981107@gmail.com>
2023-07-08 22:40:33 +0900

    fix: change variable name from promise to hangout

--- src/components/common/NavigationBar/NavigationBar.tsx
+++ src/components/common/NavigationBar/NavigationBar.tsx
@@ -11,8 +11,8 @@ const NavigationBar = ({ focusType }: NavigationPropsType) => {
     <div className={styles.Navigation}>
       <NavItem type={NAV_LIST.home} isFocused={focusType === NAV_LIST.home} />
       <NavItem
-        type={NAV_LIST.promise}
-        isFocused={focusType === NAV_LIST.promise}
+        type={NAV_LIST.hangout}
+        isFocused={focusType === NAV_LIST.hangout}
       />
       <NavItem
         type={NAV_LIST.friend}
