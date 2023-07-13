895be255ccdd2ca3c67d39ebfc2176c9feb4b255
Gwangseo Go <rufflain918@gmail.com>
2023-07-13 15:42:40 +0900

    style: modify comments

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,12 +1,12 @@
 /**
  * 특정 주의 특정 요일에 해당하는 일자를 얻는 함수
  * @example```
- * const date = getFirstDateOfWeek(2023, 6, 0, 0);
+ * const date = getDateOfWeek(2023, 6, 0, 0);
  * console.log(`${date.getFullYear()} ${date.getMonth()} ${date.getDate()}`); // 2023 05 25
  * ```
  * @param year 연도(4자리 full year)
  * @param month 달(0~11)
- * @param week 주(0~5), overflow 가능
+ * @param week 주(0~5), 범위를 벗어나도 정상 작동 (ex: 2023, 6, 6, 0 -> 2023-08-06)
  * @param dayIndex 요일(0~6)
  */
 export const getDateOfWeek = (
