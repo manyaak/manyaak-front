891830892080806fce1d10b7cfb1ebffcc866ef7
ballsona0925@naver.com <ballsona0925@naver.com>
2023-07-28 03:35:48 +0900

    feat: add NewRequestNotification component

---
+++ src/components/home/HomeContent/NewRequestNotification/NewRequestNotification.tsx
@@ -0,0 +1,34 @@
+import { ReactComponent as ArrowDownIcon } from '@/assets/icons/ArrowDownIcon.svg';
+import * as styles from '../HomeContent.css';
+
+interface NewRequestNotificationProps {
+  type: 'hangout' | 'friend';
+  count: number;
+}
+
+const NewRequestNotification = ({
+  type,
+  count,
+}: NewRequestNotificationProps) => {
+  const checkNotifications = () => {
+    console.log('');
+  };
+
+  return (
+    <div className={styles.notiLayout}>
+      <div className={styles.notiText}>
+        <span className={styles.notiCount}>{count}</span> 건의{' '}
+        {type === 'friend' ? '친구' : '약속'} 요청이 있어요
+      </div>
+      <button
+        type="button"
+        onClick={checkNotifications}
+        className={styles.notiButton}
+      >
+        <ArrowDownIcon style={{ transform: 'rotate(270deg)' }} />
+      </button>
+    </div>
+  );
+};
+
+export default NewRequestNotification;
