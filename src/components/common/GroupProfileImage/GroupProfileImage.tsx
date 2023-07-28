fd938d7576d9dcfa33e1d141bb05e554b922e3b6
ballsona0925@naver.com <ballsona0925@naver.com>
2023-07-29 01:50:45 +0900

    feat: modify profile selecting method

--- src/components/common/GroupProfileImage/GroupProfileImage.tsx
+++ src/components/common/GroupProfileImage/GroupProfileImage.tsx
@@ -4,10 +4,7 @@ import * as styles from './GroupProfileImage.css';
 const GroupProfileImage = ({ src }: { src: string[] }) => {
   // 이미지 최대 3개 제한
   const selectLength = Math.min(3, src.length);
-  const selectedProfileImg = [];
-  for (let i = 0; i < selectLength; i += 1) {
-    selectedProfileImg.push(src[i]);
-  }
+  const selectedProfileImg = src.slice(0, selectLength);
 
   return (
     <div
