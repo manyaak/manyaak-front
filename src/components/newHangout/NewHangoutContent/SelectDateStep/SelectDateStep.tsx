00d49da8d59dc59a40c3e6a4c0aed78da32ad792
kanghyun98 <lkh981107@gmail.com>
2023-07-30 10:43:58 +0900

    feat: add SelectDate and SelectLocation step on skeleton component

---
+++ src/components/newHangout/NewHangoutContent/SelectDateStep/SelectDateStep.tsx
@@ -0,0 +1,10 @@
+interface SelectDateStepProps {
+  onMoveNextStep: () => void;
+}
+
+const SelectDateStep = ({ onMoveNextStep }: SelectDateStepProps) => (
+  <div>날짜 선택 단계</div>
+  // TODO: Footer) if select, activate
+);
+
+export default SelectDateStep;
---
+++ src/components/newHangout/NewHangoutContent/SelectLocationStep/SelectLocationStep.tsx
@@ -0,0 +1,10 @@
+interface SelectLocationStepProps {
+  onMoveNextStep: () => void;
+}
+
+const SelectLocationStep = ({ onMoveNextStep }: SelectLocationStepProps) => (
+  <div>장소 선택 단계</div>
+  // TODO: Footer) if select, activate
+);
+
+export default SelectLocationStep;
