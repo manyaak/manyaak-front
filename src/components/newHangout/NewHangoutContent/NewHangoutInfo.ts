5384d16ba76b9a3c6ea406e9acd3813e37869997
kanghyun98 <lkh981107@gmail.com>
2023-08-12 16:22:29 +0900

    feat: add step key constant

--- src/components/newHangout/NewHangoutContent/NewHangoutInfo.ts
+++ src/components/newHangout/NewHangoutContent/NewHangoutInfo.ts
@@ -1,4 +1,12 @@
-export const NewHangoutStepInfo = {
+export const NEW_HANGOUT_STEP_KEY = {
+  selectFriend: 'selectFriend',
+  selectDate: 'selectDate',
+  selectLocation: 'selectLocation',
+  check: 'check',
+  complete: 'complete',
+} as const;
+
+export const NEW_HANGOUT_STEP_INFO = {
   selectFriend: 1,
   selectDate: 2,
   selectLocation: 3,
