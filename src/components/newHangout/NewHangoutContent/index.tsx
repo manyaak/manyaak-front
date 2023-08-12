7006003f78c5d5dcdf541278ce544ab19d01da3b
kanghyun98 <lkh981107@gmail.com>
2023-08-12 16:21:59 +0900

    feat: add NewHangoutContent component

---
+++ src/components/newHangout/NewHangoutContent/index.tsx
@@ -0,0 +1,5 @@
+import NewHangoutContent from './NewHangoutContent';
+import { NEW_HANGOUT_STEP_INFO, NEW_HANGOUT_STEP_KEY } from './NewHangoutInfo';
+
+export { NEW_HANGOUT_STEP_INFO, NEW_HANGOUT_STEP_KEY };
+export default NewHangoutContent;
