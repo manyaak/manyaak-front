01a7e899b32d01577a3f14910f65692373b97004
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-29 05:48:15 +0900

    feat: add comments in `useMontlyCalendar`

--- src/hooks/useMontlyCalendar.tsx
+++ src/hooks/useMontlyCalendar.tsx
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 
+/**
+ * 월 캘린더를 사용하기 위한 훅 함수
+ * @returns year 현재 년도
+ * @returns month 현재 월
+ * @returns setBeforeMonth 캘린더에서 이전 달을 보기 위한 함수
+ * @returns setNextMonth 캘린더에서 다음 달을 보기 위한 함수
+ */
 const useMontlyCalendar = () => {
   const today = new Date();
 
