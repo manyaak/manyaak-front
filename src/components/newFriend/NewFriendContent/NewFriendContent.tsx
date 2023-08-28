87e48828b9dd7b83d1549c439664047e11e41b1e
kanghyun98 <lkh981107@gmail.com>
2023-08-29 01:24:32 +0900

    feat: add NewFriendContent component for new-friend tab

---
+++ src/components/newFriend/NewFriendContent/NewFriendContent.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import InputBar from '@/components/common/InputBar';
+import FooterButton from '@/components/common/FooterButton';
+import SelectUserList from '@/components/common/SelectUserList';
+import * as styles from './NewFriendContent.css';
+import { userDummydata } from '@/dummyData';
+
+const NewFriendContent = () => {
+  const description = '친구의 아이디를 검색해보세요';
+  const searchPlaceholder = '아이디 입력';
+  const footerBtnText = '완료';
+
+  const [searchVal, setSearchVal] = useState('');
+  const [selectedId, setSelectedId] = useState<number>();
+
+  const onSelect = (id: number) => {
+    setSelectedId(id);
+  };
+
+  const onChangeSearchVal = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputVal = e.target.value;
+    setSearchVal(inputVal);
+  };
+
+  // TODO
+  const onSearch = () => {
+    console.log('검색 동작');
+
+    setSelectedId(undefined);
+  };
+
+  const onComplete = () => {
+    console.log('완료 동작');
+  };
+
+  return (
+    <>
+      <div className={styles.contentWrapper}>
+        <div className={styles.description}>{description}</div>
+        <InputBar
+          value={searchVal}
+          handleInputChange={onChangeSearchVal}
+          onClickBtn={onSearch}
+          placeholder={searchPlaceholder}
+        />
+        <div className={styles.listWrapper}>
+          <SelectUserList
+            type="friend"
+            list={userDummydata}
+            selectedId={selectedId}
+            onSelect={onSelect}
+          />
+        </div>
+      </div>
+      <FooterButton
+        label={footerBtnText}
+        onClick={onComplete}
+        disabled={!selectedId}
+      />
+    </>
+  );
+};
+
+export default NewFriendContent;
