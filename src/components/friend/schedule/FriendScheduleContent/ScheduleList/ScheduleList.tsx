22e0b81c0492e58a84ae55831467817bc3bbf3c2
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-29 05:49:54 +0900

    feat: apply `useMontlyCalendar` in ScheduleList

--- src/components/friend/schedule/FriendScheduleContent/ScheduleList/ScheduleList.tsx
+++ src/components/friend/schedule/FriendScheduleContent/ScheduleList/ScheduleList.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react';
 import HangoutBox from '@/components/common/HangoutBox';
 import { CalenderHeader, MontlyCalendar } from '@/components/common/Calendar';
+import useMontlyCalendar from '@/hooks/useMontlyCalendar';
 import * as styles from '../FriendScheduleContent.css';
 
 import { hangoutsDummydata } from '@/dummyData';
 
-const today = new Date();
-
 // TODO  hangout dummydata 교체
 const ScheduleList = () => {
-  const [currentDate, setCurrentDate] = useState({
-    year: today.getFullYear(),
-    month: today.getMonth(),
-  });
-  const { year, month } = currentDate;
-
-  const setBeforeMonth = () => {
-    setCurrentDate((prev) => ({
-      ...prev,
-      year: month === 0 ? prev.year - 1 : prev.year,
-      month: month === 0 ? 11 : prev.month - 1,
-    }));
-  };
-
-  const setNextMonth = () => {
-    setCurrentDate((prev) => ({
-      ...prev,
-      year: month === 11 ? prev.year + 1 : prev.year,
-      month: month === 11 ? 0 : prev.month + 1,
-    }));
-  };
+  const { year, month, setBeforeMonth, setNextMonth } = useMontlyCalendar();
 
   const hangout = hangoutsDummydata[0];
 
