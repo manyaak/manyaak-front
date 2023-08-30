6cfb117f0af83a770adfe143f7c2bcc0b9a421e4
kanghyun98 <lkh981107@gmail.com>
2023-08-31 01:11:28 +0900

    feat: add SelectLocationStep

--- src/components/newHangout/NewHangoutContent/SelectLocationStep/SelectLocationStep.tsx
+++ src/components/newHangout/NewHangoutContent/SelectLocationStep/SelectLocationStep.tsx
@@ -1,14 +1,73 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import InputBar from '@/components/common/InputBar';
+import SelectLocationTab from './SelectLocationTab';
+import SelectLocationInfo from './SelectLocationInfo';
+import { SelectedPlaceInfoType } from './SelectLocationMap';
+import * as styles from './SelectLocationStep.css';
+
 interface SelectLocationStepProps {
   setValid: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const SelectLocationStep = ({ setValid }: SelectLocationStepProps) => (
-  <>
-    <div>장소 선택 단계</div>
-    <button type="button" onClick={() => setValid(true)}>
-      일단 했다 생각해
-    </button>
-  </>
-);
+// TODO: 약속 이름, 장소 데이터 저장
+const SelectLocationStep = ({ setValid }: SelectLocationStepProps) => {
+  const [hangoutName, setHangoutName] = useState('');
+  const [selectedPlaceInfo, setSelectedPlaceInfo] =
+    useState<SelectedPlaceInfoType>();
+  const [showSelectLocationTab, setShowSelectLocationTab] = useState(false);
+
+  useEffect(() => {
+    const isValid = !!hangoutName && !!selectedPlaceInfo;
+    setValid(isValid);
+  }, [hangoutName, selectedPlaceInfo]);
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputVal = e.target.value;
+    setHangoutName(inputVal);
+  };
+
+  const onToggleSelectLocationTab = () => {
+    setShowSelectLocationTab((prev) => !prev);
+  };
+
+  const onConfirmSelectLocation = (placeInfo: SelectedPlaceInfoType) => {
+    setSelectedPlaceInfo(placeInfo);
+    setShowSelectLocationTab(false);
+  };
+
+  return (
+    <>
+      <div className={styles.contentWrapper}>
+        <div className={styles.sectionWrapper}>
+          <div className={styles.title}>이름</div>
+          <InputBar
+            value={hangoutName}
+            handleInputChange={onChangeInput}
+            placeholder="약속 이름 입력"
+          />
+        </div>
+        <div className={styles.sectionWrapper}>
+          <div className={styles.title}>장소</div>
+          <div
+            className={styles.selectLocationBtn}
+            onClick={onToggleSelectLocationTab}
+          >
+            약속 장소 선택하기
+          </div>
+          {selectedPlaceInfo && (
+            <SelectLocationInfo selectedPlaceInfo={selectedPlaceInfo} />
+          )}
+        </div>
+      </div>
+      {showSelectLocationTab && (
+        <SelectLocationTab
+          onConfirm={onConfirmSelectLocation}
+          onCloseTab={onToggleSelectLocationTab}
+        />
+      )}
+    </>
+  );
+};
 
 export default SelectLocationStep;
