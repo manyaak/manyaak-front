d4f95840707c62240e0c79ec50c53ddcae529eb4
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-02 04:56:48 +0900

    feat: add create-new group api

--- src/apis/friend.ts
+++ src/apis/friend.ts
@@ -4,6 +4,7 @@ import { getApi } from '@/utils/api';
 const BASE_QUERY_KEY = ['friend'] as const;
 
 // TODO type
+
 /** 유저의 친구 목록, 그룹 목록, 친구 요청 목록을 조회하는 API */
 export const api_getFriendData = getApi('GET', '/api/friend', {
   needToLogin: true,
@@ -32,6 +33,19 @@ export const api_requestFriend = getApi('POST', '/api/request/friend', {
   needToLogin: true,
   requestSchema: z.object({
     pathParams: z.tuple([z.number().describe('request id')]),
+  }),
+  getQueryKey: () => [...BASE_QUERY_KEY, 'friend-request'],
+  _resultType: {} as any,
+});
+
+/** 그룹 생성하는 API */
+export const api_createGroup = getApi('POST', '/api/club', {
+  needToLogin: true,
+  requestSchema: z.object({
+    body: z.object({
+      clubName: z.string(),
+      friendList: z.array(z.number()),
+    }),
   }),
   getQueryKey: () => [...BASE_QUERY_KEY, 'friend-request'],
   _resultType: {} as any,
