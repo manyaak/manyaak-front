bac72f1dfba77a2f06014fd50498e286cb549296
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-01 10:17:14 +0900

    feat: add Schedule Type

--- src/types/schedule.ts
+++ src/types/schedule.ts
@@ -2,3 +2,21 @@ export interface ScheduleType {
   start: Date;
   end: Date;
 }
+
+export interface FriendType {
+  id: number;
+  name: string;
+  img: string;
+  statusMessage?: string | null;
+}
+
+export interface ScheduleInfoType {
+  scheduleId: number;
+  scheduleDate: string;
+  time: string;
+  info: string;
+  detailAddress: string;
+  lng: number;
+  lat: number;
+  friendList: FriendType[];
+}
